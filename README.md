4062327bf5282d8d6eb2249bf203a51a7103a737
131 <Netrvin@users.noreply.github.com>
2018-05-04 18:28:44 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,22 +1,76 @@
-# AntiCopy
-Prevent non-professional users from copying or printing content from page
+# PageGuard.js
+No copying, printing as well as opening developers tools.
 
-It can prevents users from :
+It can prevents user from :
 * Select
 * Print
 * Right click
 * Crtl / Shift / Alt / F12
+* Open Developers Tools (Including opening in a separate window)
 
-The script will bind the special events every 0.5 second, so the users cannot lift the restrictions easily.
+You can use CSS if you don't run Javascript. But for safety, you should use this javascript and make your page only show when the Javscript is on.
 
-No matter whether javascript is enabled or not, non-professional user cannot do the things above easily.
-
-## Installation
-
+## Uasge
+Download or
 ```
-<link href="https://netrvin.github.io/AntiCopy/anticopy.css" rel="stylesheet">
-<script type="text/javascript" src="https://netrvin.github.io/AntiCopy/anticopy.js"></script>
+<script type="text/javascript" src="https://netrvin.github.io/PageGuard.js/pageguard.min.js"></script>
 ```
+### AntiCopy
+```
+var anticopy_id = PageGuard.anticopy();
+```
+
+You can use the following codes to allow user to copy again (Cannot clear the CSS):
+```
+clearInterval(anticopy_id);
+document.oncontextmenu = null; 
+document.oncopy = null;
+document.onselectstart = null;
+document.ondragstart = null;
+document.oncopy = null;
+document.onbeforecopy = null;
+document.onkeydown = null;
+window.onhelp = null;
+```
+
+### Detect Developers Tools
+Supported:
+* Chrome (latest version : 66)
+* Opera (latest version : 52)
+* Firefox (latest version : 59) (If it opens in a separate window, it will only work when the user open the console)
+* Others (Not tested)
+
+It can only run one at the same time.
+```
+var detect_key = PageGuard.detectDevTools(function () {
+	// Your codes will run when developers tools is opening
+});
+```
+
+You can also use the following codes to stop detecting:
+```
+PageGuard.stopDetecting(detect_key);
+```
+
+## Safe Tips
+Don't let user get the anticopy_id and the detect_key.
+You can write your codes like this:
+```
+(function () {
+    // codes
+})();
+```
+
+## Addons
+### Anti Copy & Print (CSS)
+Download or
+```
+<link href="https://netrvin.github.io/PageGuard.js/anticopy.min.css" rel="stylesheet">
+```
+
 ## Examples
+https://netrvin.github.io/PageGuard.js/example.html
 
-https://netrvin.github.io/AntiCopy/test.html
+## Thanks
+https://github.com/sindresorhus/devtools-detect
+https://stackoverflow.com/questions/7798748/find-out-whether-chrome-console-is-open
---
+++ pageguard.js
@@ -0,0 +1,117 @@
+﻿/*!
+ * PageGuard.js v1.0.0 (https://github.com/Netrvin/PageGuard.js)
+ * Licensed under the MIT license
+ * Included some codes from https://github.com/sindresorhus/devtools-detect
+ * Used some codes from https://stackoverflow.com/questions/7798748/find-out-whether-chrome-console-is-open
+ */
+(function () {
+    var PageGuard = {};
+    var onDevToolsOpen = function () {};
+    var is_detecting = false;
+    var stop_key = 0;
+    var detectDevTools_id = 0;
+    var ele = 9;
+
+    var is_firefox = navigator.userAgent.indexOf('Firefox') != -1;
+
+    var element = document.createElement('any');
+    element.__defineGetter__('id', function () {
+        onDevToolsOpen();
+    });
+
+    var r = /./;
+    r.toString = function () {
+        onDevToolsOpen();
+    }
+
+    function detectDevTools() {
+
+        if (!is_firefox) {
+            console.log(element);
+            ele++;
+            if (ele == 10) {
+                console.clear();
+                ele = 0;
+                var r2 = /./;
+                r2.toString = function () {
+                    onDevToolsOpen();
+                }
+                console.log('%c', r2);
+            }
+        }
+
+        var widthThreshold = window.outerWidth - window.innerWidth > 50;
+        var heightThreshold = window.outerHeight - window.innerHeight > 200;
+        var orientation = widthThreshold ? 'vertical' : 'horizontal';
+        if (!(heightThreshold && widthThreshold) && ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)) {
+            onDevToolsOpen();
+        }
+        if (window.console && window.console.firebug) {
+            onDevToolsOpen();
+        }
+    }
+
+    PageGuard.anticopy = function () {
+        return setInterval(function () {
+            var returnFalse = function () {
+                return false;
+            };
+
+            document.oncontextmenu = returnFalse;
+            document.oncopy = returnFalse;
+            document.onselectstart = returnFalse;
+            document.ondragstart = returnFalse;
+            document.oncopy = returnFalse;
+            document.onbeforecopy = returnFalse;
+            window.onhelp = returnFalse;
+            document.onkeydown = function (event) {
+                if (event.ctrlKey) {
+                    return false;
+                }
+                if (event.altKey) {
+                    return false;
+                }
+                if (event.shiftKey) {
+                    return false;
+                }
+                if (event.keyCode == 123) {
+                    return false;
+                }
+            };
+        }, 100);
+    };
+
+    PageGuard.detectDevTools = function (func) {
+        if (!is_detecting) {
+            is_detecting = true;
+            onDevToolsOpen = func;
+            stop_key = Math.random();
+            if (is_firefox) {
+                console.log(r);
+            }
+            detectDevTools_id = setInterval(function () {
+                detectDevTools();
+            }, 100);
+            return stop_key;
+        } else {
+            return false;
+        }
+    };
+
+    PageGuard.stopDetecting = function (key) {
+        if (is_detecting) {
+            if (key == stop_key) {
+                is_detecting = false;
+                clearInterval(detectDevTools_id);
+                onDevToolsOpen = function () {};
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    };
+
+    window.PageGuard = PageGuard;
+})();
